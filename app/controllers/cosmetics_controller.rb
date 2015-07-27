7af9f1af13bcfc208ca6e87ab734cca50bdeff7b
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-07-27 22:13:04 +0900

    Show category and brand name on view of cosmetics#show

--- app/controllers/cosmetics_controller.rb
+++ app/controllers/cosmetics_controller.rb
@@ -8,5 +8,7 @@ class CosmeticsController < ApplicationController
 
   def show
     @cosmetic = Cosmetic.find_by(id: params[:id])
+    @category = Category.find_by(id: @cosmetic.category_id)
+    @brand = Brand.find_by(id: @cosmetic.brand_id)
   end
 end

88b79039bc27674dab102b8d70f076fd7f8c7a20
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-07-28 09:09:12 +0900

    Show ingredients on cosmetics#show

--- app/controllers/cosmetics_controller.rb
+++ app/controllers/cosmetics_controller.rb
@@ -10,5 +10,10 @@ class CosmeticsController < ApplicationController
     @cosmetic = Cosmetic.find_by(id: params[:id])
     @category = Category.find_by(id: @cosmetic.category_id)
     @brand = Brand.find_by(id: @cosmetic.brand_id)
+
+    @ingredients = []
+    @cosmetic.cosmetic_ingredients.each do |cosmetic_ingredient|
+      @ingredients << Ingredient.find_by(id: cosmetic_ingredient.ingredient_id)
+    end
   end
 end

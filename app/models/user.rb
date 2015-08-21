188bc497f8f3df93d1b5e38fcabbb506db57bb00
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-08-21 14:47:56 +0900

    has_secure_password introduced

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+
+  has_secure_password
+
+  before_save { self.email = email.downcase }
 end
